2dd9d9927391a056f94ee905330a740fc14d3ea2
Sergej Müller <translate3d@gmail.com>
2020-11-15 15:50:37 +0100

    Add ServiceWorker

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'vault-v1';
+
+self.addEventListener('fetch', event => {
+    const request = event.request;
+    const url = new URL(request.url);
+
+    if (request.method !== 'GET') {
+        return;
+    }
+
+    if (url.origin !== location.origin) {
+        return;
+    }
+
+    const ext = url.pathname.split('.').pop();
+    const whitelist = ['font', 'style', 'script', 'image'];
+
+    let destination = request.destination;
+
+    if ( whitelist.indexOf(destination) === -1 ) {
+        return;
+    }
+
+    event.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(request).then( response => {
+                return response || fetch(request).then( response => {
+                    if ( !response || response.status !== 200 || response.type !== 'basic' ) {
+                        return response;
+                    }
+
+                    cache.put(request, response.clone());
+                    return response;
+                } );
+            } );
+        })
+    );
+} );
